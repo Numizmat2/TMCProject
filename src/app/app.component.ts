b651133794ffe3c767e75cac36bd59e46e5f9a92
Bartosz_Pingot <pingotbartosz13@gmail.com>
2019-05-16 19:30:23 +0200

    Parsing and displayng xml response from wms server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,36 @@ export class AppComponent {
 
   constructor(private dataService: DataService) {}
   response;
+  error;
   private observable: Observable<any>;
 
   sendData(minx, miny, maxx, maxy) {
     const inputValues = minx + ',' + miny + ',' + maxx + ',' + maxy;
     this.observable = this.dataService.getPropertyData(inputValues);
     this.observable.subscribe((res) => {
-      this.response = res;
+      this.parseResponse(res);
     });
+  }
+
+  parseResponse(res) {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(res, 'text/xml');
+    const elems = xml.getElementsByTagName('Attribute');
+    if (elems.item(0).innerHTML !== 'Usługa nie udostępnia danych opisowych dla wybranego obiektu') {
+      const id = 'Identyfikator działki: ' + elems.item(0).innerHTML + '\n';
+      const woj = 'Województwo: ' + elems.item(1).innerHTML + '\n';
+      const pow = 'Powiat: ' + elems.item(2).innerHTML + '\n';
+      const gmin = 'Gmina: ' + elems.item(3).innerHTML + '\n';
+      const ob = 'Obręb: ' + elems.item(4).innerHTML + '\n';
+      const nr = 'Numer działki: ' + elems.item(5).innerHTML + '\n';
+      const kw = 'KW: ' + elems.item(6).innerHTML + '\n';
+      const date = 'Data publikacji: ' + elems.item(7).innerHTML + '\n';
+      const inf = 'Informacje dodatkowe o działce: ' + elems.item(8).innerHTML + '\n';
+
+      const results = [id, woj, pow, gmin, ob, nr, kw, date, inf];
+      this.response = results;
+    } else {
+      this.error = 'Usługa nie udostępnia danych opisowych dla wybranego obiektu';
+    }
   }
 }
